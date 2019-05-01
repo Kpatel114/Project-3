97221aebe8b2998a709c3ea980a4e3f7d59d8721
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-04-30 21:51:23 -0500

    Added javascript image slider, updated media queries in css

--- Java.js
+++ Java.js
@@ -4,4 +4,24 @@ function initMap() {
 		center: {lat: 41.878113, lng: -87.629799},
 		zoom: 8
 	});
+<<<<<<< HEAD
 }
+const carouselSlide = document.querySelector('carousel-slide');
+const carouselImages = document.querySelectorAll('carousel-slide img')
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListener(
+=======
+}
+>>>>>>> a6a470b3a2524f4bf315f1f10ab4475d0bc34692
