80ed821695675456a3ed779b0345ba6d442e98e6
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-04-30 23:48:56 -0500

    added new map/image slider and new css, html and js files

--- Java.js
+++ Java.js
@@ -1,27 +1,11 @@
-var map;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
+	var map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 41.878113, lng: -87.629799},
-		zoom: 8
+		zoom: 10
 	});
-<<<<<<< HEAD
-}
-const carouselSlide = document.querySelector('carousel-slide');
-const carouselImages = document.querySelectorAll('carousel-slide img')
-
-//Buttons
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
-//Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-//Button Listeners
-
-nextBtn.addEventListener(
-=======
-}
->>>>>>> a6a470b3a2524f4bf315f1f10ab4475d0bc34692
+	var Location= {lat: 41.878113,lng: -87.629799};
+  	var marker = new google.maps.Marker({
+  			position: Location,
+  			map: map,
+  			title: 'Location'
+  			});
